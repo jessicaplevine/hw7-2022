69ff1daab85e355ba11d67dcea10b3cc41d0096c
jessicaplevine <112430325+jessicaplevine@users.noreply.github.com>
2022-10-31 15:52:47 -0400

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -1,12 +1,22 @@
 var video = document.querySelector("#player1");
 
+/* turns off autoplay and loop, opening message shown in console */
 window.addEventListener("load", function() {
 	console.log("Good job opening the window")
-/* this gives you a message in the console when the page is reloaded */
+	video.autoplay = false;
+	video.loop = false
 });
 
+/* play video when button is clicked, "Play Video" shown in console, volume starts @ 100% */
 document.querySelector("#play").addEventListener("click", function() {
 	console.log("Play Video");
-	
+	document.querySelector("#volume").innerHTML = video.volume *100 + "%"
+	video.play()
 });
+
+/* pause video when button is clicked, "Pause Video" shown in console */
+document.querySelector("#pause").addEventListener("click", function(){
+	console.log("Pause Video");
+	video.pause()
+})
 
