a2a156115cc266a62c877e85e4fcff30e138a176
jessicaplevine <112430325+jessicaplevine@users.noreply.github.com>
2022-10-31 15:14:25 -0400

    Add files via upload

---
+++ js/video.js
@@ -0,0 +1,12 @@
+var video = document.querySelector("#player1");
+
+window.addEventListener("load", function() {
+	console.log("Good job opening the window")
+/* this gives you a message in the console when the page is reloaded */
+});
+
+document.querySelector("#play").addEventListener("click", function() {
+	console.log("Play Video");
+	
+});
+
