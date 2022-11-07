3b991570a79d07c1f91d5448a838e8c9f76de8c3
jessicaplevine <112430325+jessicaplevine@users.noreply.github.com>
2022-11-07 17:00:15 -0500

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -8,7 +8,7 @@ window.addEventListener("load", function() {
 });
 
 /* play video when button is clicked, "Play Video" shown in console, volume starts @ 100% */
-document.querySelector("#play").addEventListener("click", function() {
+document.querySelector("#play").addEventListener("click", function(){
 	console.log("Play Video");
 	document.querySelector("#volume").innerHTML = video.volume *100 + "%"
 	video.play()
@@ -18,5 +18,47 @@ document.querySelector("#play").addEventListener("click", function() {
 document.querySelector("#pause").addEventListener("click", function(){
 	console.log("Pause Video");
 	video.pause()
-})
+});
 
+/* changing the playback speed, current speed shown in console */
+document.querySelector("#slower").addEventListener("click", function(){
+	video.playbackRate -= video.playbackRate * 0.1
+	console.log("The current playback rate is " + video.playbackRate)
+});
+document.querySelector("#faster").addEventListener("click", function(){
+	video.playbackRate += video.playbackRate * 0.1
+	console.log("The current playback rate is " + video.playbackRate)
+});
+
+/* skipping ahead, current location shown in console */
+document.querySelector("#skip").addEventListener("click", function(){
+	if(video.duration < video.currentTime + 10){
+		video.currentTime = 0}
+	else{
+		video.currentTime += 10}
+	console.log("The current location in the video is " + video.currentTime)
+});
+
+/* mutes/unmutes video, mute button changes text */
+document.querySelector("#mute").addEventListener("click", function(){
+	if(video.muted === false){
+		video.muted = true
+		document.getElementById("mute").innerHTML = "Unmute"}
+	else{
+		video.muted = false
+		document.getElementById("mute").innerHTML = "Mute"}
+});
+
+/* volume slider with updating corresponding text below */
+document.querySelector("#slider").addEventListener("click", function(){
+	video.volume = document.querySelector("#slider").value/100
+	document.querySelector("#volume").innerHTML = video.volume *100 + "%"
+});
+
+/* Old School vs. Original view */
+document.querySelector("#vintage").addEventListener("click", function(){
+	video.classList.add("oldSchool");
+});
+document.querySelector("#orig").addEventListener("click", function(){
+	video.classList.remove("oldSchool");
+});
